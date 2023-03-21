7d4075e40f165b3e8d4e9c11809ded98b006289d
LivailleDev <livaille.dev@gmail.com>
2023-03-21 15:26:41 +0000

    Refresh ONE - start here MASTER

---
+++ src/pages/Home/index.js
@@ -0,0 +1,15 @@
+import { View, Text, StyleSheet } from 'react-native'
+
+export default function Home(){
+    return(
+        <View style={styles.container}>
+            <Text>Home Page</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+    },
+})
